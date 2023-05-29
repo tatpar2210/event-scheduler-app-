232c5a06c70842cd0f8ab07b800c10750b856b4c
Tatpar Mishra <2210tatpar@gmail.com>
2023-05-29 23:17:00 +0530

    hashrouter commit

--- src/app/app.js
+++ src/app/app.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route, HashRouter } from "react-router-dom";
 import Header from "./Header/Header";
 import Callender from "./Callender/Callender";
 
 
 function App() {
   return(
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Header />}>
           <Route index element={<Callender />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
 
   )
 }
